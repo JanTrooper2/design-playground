b2a068ddd3888bc9491f51848bbd5f7957660be1
JanTrooper2 <JanGoergens@gmail.com>
2021-05-05 17:34:23 +0200

    implementation of the Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import FireshipNav from './components/FireshipNav.js'
 import RandomText from './components/RandomText.js'
+import FeedbackForm from './components/FeedbackForm.js'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <FireshipNav></FireshipNav>
-      <main>
-        <RandomText></RandomText>
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <FireshipNav />
+        <main className="content">
+          <Switch>
+            <Route exact path="/">
+              <RandomText />
+            </Route>
+            <Route exact path="/feedback">
+              <FeedbackForm />
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 export default App;
---
+++ src/components/FeedbackForm.js
@@ -0,0 +1,9 @@
+const FeedbackForm = (props) => {
+  return (
+    <div>
+      <h2>Feedback Form</h2>
+    </div>
+  );
+}
+
+export default FeedbackForm
