eb2a1ee685e246f70275f89d4f00f434730c84ca
JanTrooper2 <JanGoergens@gmail.com>
2021-05-08 16:33:16 +0200

    more Ipsums!

--- src/components/ipsum/LoremIpsum.js
+++ src/components/ipsum/LoremIpsum.js
@@ -1,7 +1,7 @@
-const RandomText = () => {
+const LoremIpsum = () => {
   return (
     <div>
-      <h1>Random Heading</h1>
+      <h1>Lorem Ipsum</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut volutpat metus. Phasellus tempor non nunc venenatis ultrices. Sed imperdiet at nisl non bibendum. Morbi id neque enim. Cras mattis justo eget libero aliquam interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum scelerisque vel elit vel scelerisque. Etiam et urna quis nisl aliquet bibendum.</p>
       <p>Mauris interdum, enim eget elementum lacinia, ex velit scelerisque ligula, sed hendrerit ex ipsum eget turpis. Pellentesque porttitor nunc ac bibendum semper. Suspendisse faucibus finibus sapien, at sollicitudin ex convallis in. Nulla tempus luctus enim, eu cursus nisl pretium ac. Donec sed dui sollicitudin, laoreet augue at, semper libero. Cras nec erat dapibus, tempus justo in, ultricies dolor. Mauris in ex feugiat, tempor eros vel, lacinia erat.</p>
       <p>Nam vestibulum feugiat ipsum, eu pharetra quam accumsan id. Vivamus ultrices mauris at urna porta dignissim. Etiam condimentum nulla et turpis egestas dignissim. Maecenas in metus ac metus vehicula vestibulum eu et lacus. Nunc a eros ligula. Ut in ipsum sed metus rutrum vulputate ac in metus. Nullam volutpat posuere condimentum. Integer dapibus, augue lacinia pulvinar elementum, erat tellus cursus enim, vel dapibus augue nisl in magna. Nunc justo mi, bibendum ut luctus vel, luctus et eros. Morbi cursus massa urna, eu rhoncus nisl imperdiet non. Morbi tincidunt maximus mauris, et luctus est lacinia posuere. Pellentesque consequat, augue sed commodo feugiat, eros velit vulputate metus, eget vehicula felis elit quis nibh. Curabitur eu vulputate ante. Aliquam commodo dolor ut eleifend congue. Quisque vitae elit porttitor, pulvinar orci sit amet, sollicitudin lorem. Integer id erat ut tellus auctor pellentesque sit amet vitae ex.</p>
@@ -30,4 +30,4 @@ const RandomText = () => {
   )
 }
 
-export default RandomText
+export default LoremIpsum;
