966fbb38fb86ce3fa616c22746204cd8d5d66efb
JanTrooper2 <JanGoergens@gmail.com>
2021-05-10 13:30:28 +0200

    small style changes

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -2,6 +2,7 @@ const FeedbackForm = (props) => {
   return (
     <div>
       <h2>Feedback Form</h2>
+      <p>Normal Text with a <span>spa sadfsadfasdf n</span> part.</p>
     </div>
   );
 }
--- src/components/ipsum/DogIpsum.js
+++ src/components/ipsum/DogIpsum.js
@@ -1,3 +1,5 @@
+import './DogIpsum.css'
+
 const DogIpsum = () => {
   return (
     <div>
@@ -9,6 +11,9 @@ const DogIpsum = () => {
       <p>Much ruin diet long woofer porgo shooberino fat boi wow very biscit doggorino, shoob heckin he made many woofs blop. Pupperino much ruin diet noodle horse very hand that feed shibe thicc heck pats, pupper porgo doggo bork. Doing me a frighten what a nice floof doge sub woofer porgo doge sub woofer yapper, wrinkler lotsa pats big ol heckin good boys and girls shoob.</p>
       <p>Such treat most angery pupper I have ever seen sub woofer, the neighborhood pupper. Snoot puggorino extremely cuuuuuute very good spot, ur givin me a spook maximum borkdrive shoober, pupper borkdrive. Maximum borkdrive very hand that feed shibe very good spot most angery pupper I have ever seen big ol smol borking doggo with a long snoot for pats ruff, blep heckin angery woofer big ol pupper he made many woofs. Yapper pupperino length boy borking doggo, h*ck. Snoot boof wow such tempt doggorino, dat tungg tho extremely cuuuuuute very taste wow long bois, big ol pupper most angery pupper I have ever seen. Shoob lotsa pats tungg h*ck, big ol. Borking doggo lotsa pats blop, big ol pupper. Doing me a frighten doggorino smol thicc ruff maximum borkdrive big ol pupper, porgo length boy maximum borkdrive heckin good boys and girls length boy.</p>
       <p>Smol doggorino boofers extremely cuuuuuute porgo, blop smol borking doggo with a long snoot for pats wow such tempt. Puggorino mlem tungg ruff, fat boi. Bork you are doin me a concern pupperino puggo maximum borkdrive h*ck much ruin diet, big ol porgo lotsa pats doge. very good spot dat tungg tho heckin. Fat boi heckin good boys and girls wow very biscit heck pats, very jealous pupper heckin. Shoober fat boi ur givin me a spook many pats boof wrinkler, ruff tungg heckin good boys. Very taste wow snoot doggorino heck, wow very biscit snoot. Heck smol doing me a frighten mlem, doggorino most angery pupper I have ever seen. You are doing me a frighten corgo borkdrive doggorino heck yapper, you are doing me the shock long water shoob aqua doggo wow very biscit very good spot, shoob shoober you are doin me a concern very good spot.  Fat boi you are doing me a frighten heckin angery woofer borkf tungg, long woofer snoot many pats.</p>
+      <div className="gif-container">
+        <img className="dog-gif" alt="Dog being scared" src="https://media.giphy.com/media/51Uiuy5QBZNkoF3b2Z/giphy.gif"></img>
+      </div>
     </div>
   )
 }
