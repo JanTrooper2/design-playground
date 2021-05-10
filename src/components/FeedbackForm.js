a632be8838f8bb02cd242c0ece52e285fa591b9c
JanTrooper2 <JanGoergens@gmail.com>
2021-05-10 13:59:41 +0200

    updated styling variable names

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,8 +1,10 @@
+import './FeedbackForm.css';
+
 const FeedbackForm = (props) => {
   return (
     <div>
       <h2>Feedback Form</h2>
-      <p>Normal Text with a <span>spa sadfsadfasdf n</span> part.</p>
+      <p>Normal Text with a <span>span</span> part.</p>
     </div>
   );
 }
