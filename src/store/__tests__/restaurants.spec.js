9a93cb49cb127077b376d8c80c668b795d495169
JanTrooper2 <JanGoergens@gmail.com>
2021-05-19 12:01:00 +0200

    adding loading feature

--- src/store/__tests__/restaurants.spec.js
+++ src/store/__tests__/restaurants.spec.js
@@ -4,25 +4,58 @@ import restaurantsReducer from '../restaurants/reducers';
 import {loadRestaurants} from '../restaurants/actions';
 
 describe('restaurants', () => {
+  let store;
+  describe('initially', () => {
+    it('does not have the loading flag set', () => {
+      const initialState = {};
+
+      const store = createStore(
+        restaurantsReducer,
+        initialState,
+        applyMiddleware(thunk),
+      );
+
+      expect(store.getState().loading).toEqual(false);
+    });
+  });
+
   describe('loadRestaurants action', () => {
-    it('stores the restaurants', async () => {
-      const records = [
-        {id: 1, name: 'Sushi Place'},
-        {id: 2, name: 'Pizza Place'},
-      ];
+    beforeEach(() => {
       const api = {
-        loadRestaurants: () => Promise.resolve(records),
+        loadRestaurants: () => new Promise(() => {}),
       };
       const initialState = {
         records: [],
       };
-      const store = createStore(
+      store = createStore(
         restaurantsReducer,
         initialState,
         applyMiddleware(thunk.withExtraArgument(api)),
       );
-      await store.dispatch(loadRestaurants());
-      expect(store.getState().records).toEqual(records);
+      return store.dispatch(loadRestaurants());
+    });
+
+    describe('when loading succeeds', () => {
+      const records = [
+        {id: 1, name: 'Sushi Place'},
+        {id: 2, name: 'Pizza Place'},
+      ];
+
+      it('stores the restaurants', () => {
+        expect(store.getState().records).toEqual(records);
+      });
+
+      it('clears the loading flag', () => {
+        expect(store.getState().loading).toEqual(false);
+      });
+    });
+  });
+
+  describe('while loading', () => {
+    it('sets a loading flag', () => {
+      store.dispatch(loadRestaurants());
+
+      expect(store.getState().loading).toEqual(true);
     });
   });
 });
