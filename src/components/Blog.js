5b13051580c02bd1ceba3f969abd40acc9e40144
JanTrooper2 <JanGoergens@gmail.com>
2021-06-07 20:31:00 +0200

    Blog now shows entries

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,25 +3,30 @@ import React, {useState} from 'react';
 import '../styles/Blog.css';
 
 const Blog = () => {
-  const fetchNotes = axios
-    .get('http://localhost:5000/blogs')
-    .then(response => response.data);
-  // console.log(fetchNotes);
-  const notesArr = fetchNotes.then(res =>
-    res.map(note => {
+  const [loading, setLoading] = useState(true);
+  const convertPromiseToValue = async () => {
+    let cachedNotes = await fetchNotes;
+    cachedNotes = cachedNotes.map(note => {
       return (
-        <figure>
+        <figure key={note.id}>
           <h3>{note.title}</h3>
           <p>{note.body}</p>
         </figure>
       );
-    }),
-  );
-  console.log(notesArr);
+    });
+    setNotes(cachedNotes);
+    setLoading(false);
+  };
+  const fetchNotes = axios
+    .get('http://localhost:5000/blogs')
+    .then(response => response.data);
+
+  const [notes, setNotes] = useState(convertPromiseToValue);
+
   return (
     <section className="blog">
       <h1>Blog</h1>
-      {/* {notesArr} */}
+      {!loading && notes}
     </section>
   );
 };
