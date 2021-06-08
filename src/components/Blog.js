27f4fd32956e005afb916bf1c0722c5dcd563260
JanTrooper2 <JanGoergens@gmail.com>
2021-06-08 18:36:17 +0200

    removed unused vars

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import React, {useState} from 'react';
+// import axios from 'axios';
+import React from 'react';
 import '../styles/Blog.css';
 
 const Blog = props => {
