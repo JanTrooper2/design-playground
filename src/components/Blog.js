27545982a9f35dcd523342c2e2ae8ddd2090d7c0
JanTrooper2 <JanGoergens@gmail.com>
2021-06-08 17:06:07 +0200

    improved Blog tests

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,31 +2,32 @@ import axios from 'axios';
 import React, {useState} from 'react';
 import '../styles/Blog.css';
 
-const Blog = () => {
-  const [loading, setLoading] = useState(true);
-  const convertPromiseToValue = async () => {
-    let cachedNotes = await fetchNotes;
-    cachedNotes = cachedNotes.map(note => {
-      return (
-        <figure key={note.id}>
-          <h3>{note.title}</h3>
-          <p>{note.body}</p>
-        </figure>
-      );
-    });
-    setNotes(cachedNotes);
-    setLoading(false);
-  };
-  const fetchNotes = axios
-    .get('http://localhost:5000/blogs')
-    .then(response => response.data);
+const Blog = props => {
+  // const [loading, setLoading] = useState(true);
+  // const convertPromiseToValue = async () => {
+  //   let cachedNotes = await fetchNotes;
+  //   cachedNotes = cachedNotes.map(note => {
+  //     return (
+  //       <figure className={'blogEntry note-' + note.id} key={note.id}>
+  //         <h3>{note.title}</h3>
+  //         <p>{note.body}</p>
+  //       </figure>
+  //     );
+  //   });
+  //   setNotes(cachedNotes);
+  //   setLoading(false);
+  // };
+  // const fetchNotes = axios
+  //   .get('http://localhost:5000/blogs')
+  //   .then(response => response.data);
 
-  const [notes, setNotes] = useState(convertPromiseToValue);
+  // const [notes, setNotes] = useState(convertPromiseToValue);
 
   return (
     <section className="blog">
       <h1>Blog</h1>
-      {!loading && notes}
+      {props.children}
+      {/* {!loading && notes} */}
     </section>
   );
 };
--- src/components/__tests__/Blog.spec.js
+++ src/components/__tests__/Blog.spec.js
@@ -1,17 +1,41 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
 import {render, act} from '@testing-library/react';
-// import flushPromises from 'flush-promises';
-// import userEvent from '@testing-library/user-event';
 import Blog from '../Blog';
 
 describe('Blog', () => {
-  let context;
+  const entry = {
+    title: 'Test Blog entry',
+    body: 'Lorem Ipsum',
+    author: 'test-application',
+    id: 1,
+  };
+  const BlogEntry = ({entry}) => {
+    return (
+      <figure className={'blogEntry entry-' + entry.id} key={entry.id}>
+        <h3>{entry.title}</h3>
+        <p>{entry.body}</p>
+      </figure>
+    );
+  };
+  let component;
   beforeEach(() => {
-    context = render(<Blog />);
+    component = render(
+      <Blog>
+        <BlogEntry entry={entry}></BlogEntry>
+      </Blog>,
+    );
   });
   describe('default setup', () => {
     it('renders the heading', () => {
-      const {queryByText} = context;
-      // expect(queryByText).toMatch('Blog');
+      expect(component.container).toHaveTextContent('Blog');
+    });
+    it('contains sample entries', () => {
+      // console.log(component.container);
+      // console.log(component.getByText('Blog'));
+      // console.log(component.container.querySelector('.entry-1'));
+
+      expect(component.container).toHaveTextContent('Test Blog entry');
     });
   });
 });
